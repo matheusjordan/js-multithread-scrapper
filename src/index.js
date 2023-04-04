6069a39dae18787fdc287495971e85b150a36fff
matheusjordan <matheusj1599@gmail.com>
2023-04-04 19:49:10 -0300

    Adicionado extrator de links

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {HTMLReader} from "./services/html-reader.js";
 
 const main = async () => {
     const page = await HTMLReader.readFromFile('./tests/campina-grande-example.html');
+    const links = HTMLReader.linksExtractor(page);
+    console.log(links)
 }
 
 main();
--- src/services/html-reader.js
+++ src/services/html-reader.js
@@ -1,12 +1,36 @@
 'use strict';
 import fs from 'fs/promises';
+import {JSDOM as jsdom} from 'jsdom'
 
 export class HTMLReader {
     static async readFromFile(path) {
         try {
             return await fs.readFile(path, 'utf8');
         } catch (error) {
-            return new Error();
+            return new Error(error);
         }
     }
+
+    static async linksExtractor(page) {
+        // const dom = (await jsdom.fromFile(page))
+        try {
+            // const page = dom.window;
+            // const html = page.body.innerHTML;
+            const html = page;
+
+            const regex = new RegExp(/href="(https?:\/\/[^"]*)"/g);
+
+            const links = [];
+
+            let match;
+            while ((match = regex.exec(html)) !== null) {
+                links.push(match[1]);
+            }
+
+            return links;
+
+        } catch (error) {
+            return new Error(error);
+        }
+    } 
 }
