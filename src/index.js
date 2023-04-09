3a7f777e9ac58bf7364f844cf3f2a3214c46a718
matheusjordan <matheusj1599@gmail.com>
2023-04-09 10:37:23 -0300

    Added link finder by url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 'use strict';
 import {HTMLReader} from "./services/html-reader.js";
+import {UrlIndex} from "./services/url-index.js";
 
 const main = async () => {
-    const page = await HTMLReader.readFromFile('./tests/campina-grande-example.html');
+    // const page = await HTMLReader.read('./tests/campina-grande-example.html');
+
+    const url = 'https://pt.wikipedia.org/wiki/Campina_Grande';
+    const links = await search(url);
+}
+
+const search = async (url) => {
+    const page = await HTMLReader.readFromURL(url);
     const links = HTMLReader.linksExtractor(page);
-    console.log(links)
+    // const wikiLinks = HTMLReader.wikipediaLinksFinder(links);
+
+    UrlIndex.indexUrl(url, links);
+
+    console.log(`Para o link: ${url}\nForam encontrados ${links.length} links.\n\n`);
+
+
+    return links;
 }
 
 main();
--- src/services/html-reader.js
+++ src/services/html-reader.js
@@ -3,21 +3,35 @@ import fs from 'fs/promises';
 import {JSDOM as jsdom} from 'jsdom'
 
 export class HTMLReader {
+    /* Q1: método/função que lê o conteúdo HTML de uma URL */
     static async readFromFile(path) {
         try {
+            
             return await fs.readFile(path, 'utf8');
         } catch (error) {
             return new Error(error);
         }
     }
 
-    static async linksExtractor(page) {
-        // const dom = (await jsdom.fromFile(page))
+    static async readFromURL(url) {
         try {
-            // const page = dom.window;
-            // const html = page.body.innerHTML;
+            const loadedPage = await jsdom.fromURL(url, {
+                userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+            });
+
+            return loadedPage.serialize()
+
+        } catch (error) {
+            return new Error(error);
+        }
+    }
+
+    /* Q3: método/função que extrai todos os links de uma página HTML */
+    static linksExtractor(page) {
+        try {
             const html = page;
 
+            /* Q2: expressão regular para identificar links */
             const regex = new RegExp(/href="(https?:\/\/[^"]*)"/g);
 
             const links = [];
@@ -32,5 +46,5 @@ export class HTMLReader {
         } catch (error) {
             return new Error(error);
         }
-    } 
+    }
 }
