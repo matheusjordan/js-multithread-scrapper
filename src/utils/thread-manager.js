0f87cd87dbcdadf0d02196c508eada6df179f923
matheusjordan <matheusj1599@gmail.com>
2023-04-09 23:01:14 -0300

    Adicionado busca por nivel de thread

--- src/utils/thread-manager.js
+++ src/utils/thread-manager.js
@@ -1,13 +1,30 @@
 import { Worker } from 'node:worker_threads';
 
-export const run = (url) => {
+let MAX_LEVEL = 3;
+let COUNT_LINKS = 0;
+
+
+export const run = async (url) => {
+    let level = 0;
 
     /* Q5 criar uma nova thread para link encontrado */
     const thread = new Worker('./src/services/task.js');
         
     thread.once("message", (links) => {
-        console.log(links);
-        console.log(`\n\nFinalizado thread de ID ${thread.threadId}\n\n`);
+
+        if (level < MAX_LEVEL) {
+            for (let link of links) {
+                COUNT_LINKS++;
+
+                if (COUNT_LINKS < 5) {
+                    run(link);
+                }
+            }
+            level++;
+        } else  {
+            COUNT_LINKS = 0;
+        }
+        // console.log(`\n\nFinalizado thread de ID ${thread.threadId} no nível ${level}\n\n`);
     });
 
     thread.on("error", err => {
