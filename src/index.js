4af2d0628827b5e18d11899e8d678e835c106065
matheus jordan <matheusj1599@gmail.com>
2023-06-14 17:17:10 -0300

    - Added comand line for crawler script - Added scripts for package.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import {run} from "./utils/thread-manager.js";
 
+const url = process.argv[2];
+if (!url) {
+  console.error('Please set an url as argument of this comand');
+  process.exit(1);
+}
 
 const singleMain = async () => {
-    run('https://pt.wikipedia.org/wiki/Campina_Grande');
+    run(url);
 }
 
 import { WorkerPool } from './services/worker-pool.js';
 const poolMain = async () => {
     const pool = new WorkerPool(8);
-    pool.run('https://pt.wikipedia.org/wiki/Campina_Grande')
+    pool.run(url);
 }
 
 // singleMain();
